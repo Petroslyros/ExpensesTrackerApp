1760818a92596735f23324337bd35cfb7f24868f
Petros <petrosluros@gmail.com>
2025-10-16 13:47:51 +0300

    fix: removed token auth from usercontroller

---
+++ ExpensesTrackerApp/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using ExpensesTrackerApp.DTO;
+using ExpensesTrackerApp.Exceptions;
+using ExpensesTrackerApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesTrackerApp.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly IConfiguration configuration;
+
+        public UsersController(IApplicationService applicationService, IConfiguration configuration) :
+            base(applicationService)
+        {
+            this.configuration = configuration;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserReadOnlyDTO>> GetUserById(int id)
+        {
+            UserReadOnlyDTO userReadOnlyDTO = await applicationService.UserService.GetUserByIdAsync(id)
+                ?? throw new EntityNotFoundException("User", "User with id " + id + " not found");
+
+            return Ok(userReadOnlyDTO);
+        }
+
+
+
+    }
+}
