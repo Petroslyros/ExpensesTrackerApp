bb3be0a68758d298fdfa73b47f552b283051aabb
Petros <petrosluros@gmail.com>
2025-10-17 20:13:17 +0300

    fix: Mapper Config

--- ExpensesTrackerApp/Configuration/MapperConfig.cs
+++ ExpensesTrackerApp/Configuration/MapperConfig.cs
@@ -9,7 +9,12 @@ namespace ExpensesTrackerApp.Configuration
         public MapperConfig()
         {
             CreateMap<User, UserReadOnlyDTO>().ReverseMap();
-
+            // For registration map the properties that match
+            CreateMap<UserRegisterDTO, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Password, opt => opt.Ignore())
+                .ForMember(dest => dest.UserRole, opt => opt.Ignore());
         }
     }
 }
+
