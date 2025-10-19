85a8ce1b41268ec127fc82639fd03e8241156830
Petros <petrosluros@gmail.com>
2025-10-19 11:13:52 +0300

    feat: added mapper methods for register user and readonly to user entity

--- ExpensesTrackerApp/Configuration/MapperConfig.cs
+++ ExpensesTrackerApp/Configuration/MapperConfig.cs
@@ -14,6 +14,10 @@ namespace ExpensesTrackerApp.Configuration
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.Password, opt => opt.Ignore())
                 .ForMember(dest => dest.UserRole, opt => opt.Ignore());
+            CreateMap<UpdateUserDTO, User>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            //The .ForAllMembers(...) condition ensures null fields won’t overwrite existing data if
+            //they aren’t provided (useful for PATCH-like behavior).
         }
     }
 }
